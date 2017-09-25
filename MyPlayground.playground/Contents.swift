0678da6c20aad4548e15e5121495a81df244f70d
X1Warrior <zsto7638@gmail.com>
2017-09-25 13:52:14 -0600

    Tryed out some strings

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -2,4 +2,31 @@
 
 import UIKit
 
-var str = "Hello, playground"
+var myName :String
+var otherName = "Words"
+var thisName :String = "thor"
+
+
+let multiplier = 3
+let message = "\(multiplier) times 2.5 is \(Double(multiplier) * 2.5)"
+// message is "3 times 2.5 is 7.5"
+
+let unusualMenagerie = "Koala, Snail, Penguin, Dromedary"
+print("UnusualMenagerie has \(unusualMenagerie.characters.count)")
+// Prints "unusualMenagerie has 40 characters"
+
+let quotation = "We're a lot alike, you and I."
+let sameQuotation = "We're a lot alike, you and I."
+if quotation == sameQuotation {
+    print("These two strings are considered equal")
+}
+// Prints "These two strings are cconsidered equal"
+
+var someString = "some string literal value"
+
+var age = 2315
+var speed = 654.324536
+var rate = Double (age)/speed
+
+var 
+		
