5f0374754e9d198fbf66290393abd6d56fe9d891
X1Warrior <zsto7638@gmail.com>
2017-09-27 11:54:19 -0600

    Added a playound for method testing.

---
+++ Methods.playground/Contents.swift
@@ -0,0 +1,18 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+public func isReallySimple() -> Void
+{
+    print("my boss is really annoying")
+}
+isReallySimple()
+
+public func aBitLessSimple(name: String) -> Void
+{
+    let answer = "My name is not " + name
+    print(answer)
+}
+aBitLessSimple(name: "Slim Shady")
+let words = "marshall Mathers"
+aBitLessSimple(name: words)
