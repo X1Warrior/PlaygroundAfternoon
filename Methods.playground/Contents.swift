ee08003ae2fa06941090ea3897897e004f3cd0ca
X1Warrior <zsto7638@gmail.com>
2017-09-28 12:12:30 -0600

    Practice with loops

--- Methods.playground/Contents.swift
+++ Methods.playground/Contents.swift
@@ -16,3 +16,33 @@ public func aBitLessSimple(name: String) -> Void
 aBitLessSimple(name: "Slim Shady")
 let words = "marshall Mathers"
 aBitLessSimple(name: words)
+
+public func namingExample(outerName innerName :String) -> Void
+{
+    print("The inner name is: \(innerName)")
+}
+namingExample(outerName: "Zach")
+
+public func counting() -> Int
+{
+    return (2)
+}
+print("I am not \(counting()) years old")
+
+if(counting() < 10)
+{
+    print("math works in swift too")
+}
+else
+{
+    print("not very likely right now")
+}
+var test = 0
+
+while (test < counting())
+{
+    print("hahaha")
+    test += 1
+}
+
+print("yay!")
