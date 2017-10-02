a0f21b725d1a8a1ea67d68406dd799bae9f0b263
X1Warrior <zsto7638@gmail.com>
2017-10-02 12:50:28 -0600

    Demo with soe sample classes and methods.

--- NewPlayground.playground/Contents.swift
+++ NewPlayground.playground/Contents.swift
@@ -11,6 +11,43 @@ public class SimpleClass
     public init()
     {
         name = "My name is barthalemu"
+   }
+    
+    public func getName() -> String
+    {
+        return name
     }
 }
+var sample = SimpleClass()
+print(sample.getName())
 
+public class OtherClass
+{
+    private var favoriteNumber :Int
+    private var favoriteWord :String
+    
+    public init()
+    {
+        favoriteNumber = Int()
+        favoriteWord = String()
+        
+    }
+    
+    public init(favoriteNumber :Int, favoriteWord :String)
+    {
+        self.favoriteNumber = favoriteNumber
+        self.favoriteWord = favoriteWord
+    }
+    
+    public func changeInternalState() -> Void
+    {
+       self.favoriteNumber *= 1238
+       self.favoriteWord += " and more and more"
+    }
+}
+var secondClass = OtherClass()
+//Jaa would be OtherClass sample = new OtherClass();
+var thirdSample = OtherClass(favoriteNumber: 876543, favoriteWord: "stuff")
+secondClass.changeInternalState()
+thirdSample.changeInternalState()
+thirdSample.changeInternalState()
