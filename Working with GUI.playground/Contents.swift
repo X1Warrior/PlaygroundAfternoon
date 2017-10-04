be616be2f1f332b01edcecf60bca659820c9bf41
X1Warrior <zsto7638@gmail.com>
2017-10-04 13:49:19 -0600

    Worked with labels and making buttons do things.

---
+++ Working with GUI.playground/Contents.swift
@@ -0,0 +1,24 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Working with Gui :D"
+
+var myFirstButton = UIButton(frame: CGRect(x:30, y:60, width: 600, height: 120))
+
+myFirstButton.setTitle("words", for: .normal)
+
+myFirstButton.backgroundColor = .green
+
+var touchDown: UIControlEvents
+
+addTarget(_, target: Any?, action: Selector, for controlEvents: UIControlEvents)
+
+func doSomething(sender: UIButton, forEvent event: UIEvent)
+
+var myFirstLabel = UILabel(frame: CGRect(x:10, y:20, width:200, height: 40))
+
+myFirstLabel.text = "choco milk my dude"
+
+myFirstLabel.textColor = .blue
+
